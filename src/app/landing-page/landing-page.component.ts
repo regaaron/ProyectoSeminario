def560816cad3c655943ac2a4caa00db29c45f55
aaron lopez <aaron_lopez222@hotmail.com>
2025-09-03 23:06:36 -0600

    feat(landing-page): enhance navigation and add hero section with animated background

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { HeroComponent } from "../hero/hero.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeroComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
