13e8e41e9c8ad0e6654a41be0f1898291d35d678
Omar Reyes Ramirez <reyesomar028@gmail.com>
2025-09-10 09:10:55 -0600

    feature(examen): add new animations and fix one size to update better view

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideAnimations() // 👈 aquí activas animaciones
+  ]
+}).catch((err) => console.error(err));
