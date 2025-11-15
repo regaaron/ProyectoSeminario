0468818bf77fccf38e393f59118bfbe5f9089fa1
aaron lopez <aaron_lopez222@hotmail.com>
2025-11-15 01:11:53 -0600

    feat(add-questions): implement AddQuestions component with CRUD functionality and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { AdminComponent } from './admin/admin.component';
 import { AddTopicsComponent } from './add-topics/add-topics.component';
 import { AddQuestionsComponent } from './add-questions/add-questions.component';
 import { adminGuard } from './guards/admin.guard';
+import { QuestionAIComponent } from './question-ai/question-ai.component';
 
 
 export const routes: Routes = [
@@ -43,6 +44,7 @@ export const routes: Routes = [
             {path: '',redirectTo: 'addTopics',pathMatch: 'full'},
             {path: 'addTopics',component: AddTopicsComponent},
             {path: 'addQuestions',component: AddQuestionsComponent},
+            {path: 'QuestionsAI',component: QuestionAIComponent},
         ]
     },
     
---
+++ src/app/services/preguntas.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreguntasService {
+
+
+  private API_URL = 'http://localhost:3000/admin/preguntas';
+  
+    constructor(private http: HttpClient) { }
+  
+    getPreguntas(): Observable<any[]> {
+      return this.http.get<any[]>(this.API_URL);
+    }
+  
+    deletePregunta(id: number) {
+    return this.http.delete(`${this.API_URL}/${id}`);
+  }
+  
+}
